1b8d44d53a51953a1d259bb303cc2ae38981e061
Max Zakharzhevskiy <maxzz@msn.com>
2021-05-26 16:16:41 -0700

    Add simple render scene

--- src/utils/render-body.ts
+++ src/utils/render-body.ts
@@ -2,4 +2,15 @@ import noise from 'simplex-noise';
 
 export function renderBody(ctx: CanvasRenderingContext2D) {
     console.log('rendered', ctx);
+    let w = ctx.canvas.width;
+    let h = ctx.canvas.height;
+
+    ctx.fillStyle = 'red';
+    ctx.fillRect(0, 0, w, h);
+
+    for (let i = 0; i < h; i+=10) {
+        ctx.fillStyle = 'tomato';
+        ctx.fillRect(0, i, w, 5);
+        console.log([0, i, w, 5]);
+    }
 }
--- src/utils/web-worker.ts
+++ src/utils/web-worker.ts
@@ -5,21 +5,22 @@ const runtime: Worker = self as any;
 function RunStuff() {
     console.log('running');
 
+    let canvasElm: HTMLCanvasElement;
     let ctx: CanvasRenderingContext2D | null;
 
     runtime.onmessage = (event: MessageEvent) => {
         console.log('got', event);
 
         if (event.data.type === 'init') {
-            let offscreen = (event.data.canvas as HTMLCanvasElement)
-            ctx = offscreen.getContext('2d');
+            canvasElm = (event.data.canvas as HTMLCanvasElement)
+            ctx = canvasElm.getContext('2d');
         }
         if (ctx) {
             renderBody(ctx);
         }
     };
 
-    runtime.postMessage('result')
+    runtime.postMessage('result');
 }
 
 RunStuff();
