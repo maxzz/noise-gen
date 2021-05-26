9b4cd329055fe34f6330c230f68ac6ba39e587e8
Max Zakharzhevskiy <maxzz@msn.com>
2021-05-25 18:06:46 -0700

    Add working worker communications

--- src/App.tsx
+++ src/App.tsx
@@ -1,13 +1,36 @@
-import React, { useState } from 'react';
+import React, { useEffect, useState } from 'react';
 import './App.css';
 import webWorker from './utils/web-worker?url';
 
-const worker = new Worker(webWorker);
+function Canvas() {
+    const canvas = React.useRef(null);
+    const worker = React.useRef<Worker>();
+
+    useEffect(() => {
+        const newWorker = new Worker(webWorker);
+        newWorker.onmessage = (event: any) => {
+            console.log(event);
+        };
+        newWorker.onerror = (event: any) => {
+            console.log('error', event);
+        };
+        newWorker.postMessage({init: 'init'});
+        worker.current = newWorker;
+        return () => {
+            worker.current?.terminate();
+        }
+    }, []);
+
+    return (
+        <canvas ref={canvas}></canvas>
+    )
+}
 
 function App() {
     return (
         <div className="App">
             <button className="text-red-600">AAA</button>
+            <Canvas />
         </div>
     );
 }
--- src/utils/web-worker.ts
+++ src/utils/web-worker.ts
@@ -1,7 +1,10 @@
 import noise from 'simplex-noise';
 
+const ctx: Worker = self as any;
+
 function RunStuff() {
     console.log('running');
+    ctx.postMessage('result')
 }
 
 RunStuff();
