1a474f873840e8b99724fa8381fd00dd24a75628
Max Zakharzhevskiy <maxzz@msn.com>
2021-05-27 01:46:08 -0700

    Fix GitHub deploy

--- vite.config.ts
+++ vite.config.ts
@@ -2,6 +2,7 @@ import { defineConfig } from 'vite'
 import reactRefresh from '@vitejs/plugin-react-refresh'
 
 // https://vitejs.dev/config/
-export default defineConfig({
-  plugins: [reactRefresh()]
-})
+export default ({command}) => defineConfig({
+    base: '',
+    plugins: [reactRefresh()]
+  })
