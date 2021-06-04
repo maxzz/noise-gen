c254cdb18b37a16f83bf6c9b645eddafdf856c94
Max Zakharzhevskiy <maxzz@msn.com>
2021-06-03 17:36:37 -0700

    WIP: add preview. 6. conver blob from canvas. OK

--- src/components/RenderCanvas.tsx
+++ src/components/RenderCanvas.tsx
@@ -5,33 +5,6 @@ import { renderParamsAtom } from '../atoms';
 import useCanvasWorker from '../hooks/useCanvasWorker';
 import DragZone from './DragZone';
 
-function encode (input: any) {
-    var keyStr = "ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz0123456789+/=";
-    var output = "";
-    var chr1, chr2, chr3, enc1, enc2, enc3, enc4;
-    var i = 0;
-
-    while (i < input.length) {
-        chr1 = input[i++];
-        chr2 = i < input.length ? input[i++] : Number.NaN; // Not sure if the index 
-        chr3 = i < input.length ? input[i++] : Number.NaN; // checks are needed here
-
-        enc1 = chr1 >> 2;
-        enc2 = ((chr1 & 3) << 4) | (chr2 >> 4);
-        enc3 = ((chr2 & 15) << 2) | (chr3 >> 6);
-        enc4 = chr3 & 63;
-
-        if (isNaN(chr2)) {
-            enc3 = enc4 = 64;
-        } else if (isNaN(chr3)) {
-            enc4 = 64;
-        }
-        output += keyStr.charAt(enc1) + keyStr.charAt(enc2) +
-                  keyStr.charAt(enc3) + keyStr.charAt(enc4);
-    }
-    return output;
-}
-
 export default function Canvas({ seed, color }: { seed: string, color: string; }) {
     const canvas = React.useRef<HTMLCanvasElement>(null);
     const worker = useCanvasWorker(canvas);
@@ -52,8 +25,12 @@ export default function Canvas({ seed, color }: { seed: string, color: string; }
             worker.onmessage = (event: MessageEvent) => {
                 console.log('from worker2:', event.data);
                 if (event.data.type === 'render-blob') {
-                    let s = 'data:image/png;base64,'+encode(event.data.blob);
-                    console.log('img', s);
+                    var reader = new FileReader();
+                    reader.onloadend = function () {
+                        var base64data = reader.result;
+                        console.log(base64data);
+                    };
+                    reader.readAsDataURL(event.data.blob);
                 }
             };
         }
@@ -80,7 +57,7 @@ export default function Canvas({ seed, color }: { seed: string, color: string; }
                 <canvas ref={canvas} className="w-full h-full"></canvas>
             </div>
             <DragZone
-                className="absolute w-5 h-5 rounded-full border-2 -bottom-2 -right-2 z-10 
+                className="absolute w-5 h-5 rounded-full border-2 -bottom-2 -right-2 z-10
                     bg-green-500 border-green-700 active:border-green-600
                     transform active:scale-0"
                 size={manualSize} setSize={manualSizeSet} onActivated={(active: boolean) => setDragging(active)}
