a6010b8e587969611a1ac32c036d0666757a640a
Max Zakharzhevskiy <maxzz@msn.com>
2021-06-07 02:13:17 -0700

    Feature: add 9 size icon good enough for now

--- src/components/PresetSizeIcons.tsx
+++ src/components/PresetSizeIcons.tsx
@@ -12,18 +12,20 @@ function SizeIcon({ select, step }: { select: () => void, step: number; }) {
             onClick={select}
         >
             {/* border rounded-sm border-gray-400 text-gray-400 */}
-            <svg className="h-5 w-5" viewBox="0 0 24 24" fill="currentColor" stroke="purple" strokeWidth={.4} >
-                {/* <path fill="#0f0" d="M.5.5h23v23H.5z" /> */}
+            <svg className="h-5 w-5" viewBox="0 0 24 24" fill="currentColor" >
+                {/* Outer */}
+                {step === 3 && <path fill="#d6d6d6" d="M.5.5h23v23H.5z" />}
                 <path d="M23 1v22H1V1h22m1-1H0v24h24V0z" />
 
-                {/* <path fill="#0f0" d="M2.6 2.6h18.7v18.7H2.6z" /> */}
-                <path d="M20.8 3.2v17.7H3.2V3.2h17.6m1.1-1.1H2.1v19.8h19.8V2.1z" />
+                {step === 2 && <path fill="#d6d6d6" d="M2.6 2.6h18.7v18.7H2.6z" />}
+                {step === 2 && <path d="M20.8 3.2v17.7H3.2V3.2h17.6m1.1-1.1H2.1v19.8h19.8V2.1z" />}
 
-                {/* <path fill="#0f0" d="M5 5h14.1v14.1H5z" /> */}
-                <path d="M18.5 5.5v13.1h-13V5.5h13m1-1h-15v15.1h15.1V4.5h-.1z" />
+                {step === 1 && <path fill="#d6d6d6" d="M5 5h14.1v14.1H5z" />}
+                {step === 1 && <path d="M18.5 5.5v13.1h-13V5.5h13m1-1h-15v15.1h15.1V4.5h-.1z" />}
 
-                {/* <path fill="#0f0" d="M7.2 7.2h9.7v9.7H7.2z" /> */}
-                <path d="M16.3 7.7v8.7H7.7V7.7h8.6m1-1H6.7v10.7h10.7V6.7h-.1z" />
+                {/* Inner */}
+                {step === 0 && <path fill="#d6d6d6" d="M7.2 7.2h9.7v9.7H7.2z" />}
+                {step === 0 && <path d="M16.3 7.7v8.7H7.7V7.7h8.6m1-1H6.7v10.7h10.7V6.7h-.1z" />}
             </svg>
         </div>
     );
