5628171539f48e696b6ebd13edbbe377d1d9d95d
Max Zakharzhevskiy <maxzz@msn.com>
2021-06-11 00:32:46 -0700

    WIP: add canvas background switch. 6

--- src/components/ColorBgPicker.tsx
+++ src/components/ColorBgPicker.tsx
@@ -3,6 +3,8 @@ import React from 'react';
 import { ColorCanvasAtom } from '../atoms';
 
 const bgColors = ['white', 'black', 'transparent'];
+const bgTitle = `Perview canvas background: white/black/transparent.
+The saved image will have a transparent background.`;
 
 function ColorBgPicker() {
     const [backgorund, setBackgorund] = useAtom(ColorCanvasAtom);
@@ -15,11 +17,12 @@ function ColorBgPicker() {
             className="w-8 h-8 rounded border-gray-500 transform active:scale-[.97] relative"
             onClick={changeBackground}
             style={{borderWidth: backgorund === 'transparent' ? 1 : 0}}
+            title={bgTitle}
         >
-            <div className="absolute border border-gray-500 rounded-[0.15rem] inset-[2px]" ></div>
+            <div className="absolute border border-gray-500 rounded-[0.15rem] inset-[2px]"></div>
 
             <div className="py-2 h-full flex items-center justify-evenly border-2 border-gray-500 rounded"
-                title={`Canvas background: white/black/transparent`}
+
                 style={{borderColor: backgorund}}
             >
                 <div className="w-1.5 h-1/2 bg-white"></div>
