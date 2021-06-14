ebd47fb1d41e26b9ef632623226d50e6e4a5c1e4
Max Zakharzhevskiy <maxzz@msn.com>
2021-06-14 16:56:17 -0700

    WIP: generated image as bkg. 3. update layout

--- src/components/Sliders.tsx
+++ src/components/Sliders.tsx
@@ -131,39 +131,40 @@ function Sliders() {
                     <PresetSizeIcons />
                 </div>
 
-                {/* Preset + */}
+                {/* Preset set as background */}
                 <div
-                    className="w-full h-8 border rounded border-gray-400 flex items-center justify-center text-gray-400
-                        transform active:scale-[.97] cursor-pointer"
+                    className="border rounded border-gray-400 flex items-center justify-center text-gray-500
+                        transform active:scale-[.97] cursor-pointer
+                        "
                     title="Set canvas image as application background"
                     onClick={setAsBackground}
                 >
-                    <svg className="h-6 w-6" fill="none" viewBox="0 0 24 24" stroke="currentColor">
-                        <path strokeLinecap="round" strokeLinejoin="round" strokeWidth={.6} d="M17 14v6m-3-3h6M6 10h2a2 2 0 002-2V6a2 2 0 00-2-2H6a2 2 0 00-2 2v2a2 2 0 002 2zm10 0h2a2 2 0 002-2V6a2 2 0 00-2-2h-2a2 2 0 00-2 2v2a2 2 0 002 2zM6 20h2a2 2 0 002-2v-2a2 2 0 00-2-2H6a2 2 0 00-2 2v2a2 2 0 002 2z" />
+                    <svg className="h-6 w-8" fill="none" viewBox="0 0 24 24" stroke="currentColor">
+                        <path strokeLinecap="round" strokeLinejoin="round" strokeWidth={.8} d="M4 16l4.586-4.586a2 2 0 012.828 0L16 16m-2-2l1.586-1.586a2 2 0 012.828 0L20 14m-6-6h.01M6 20h12a2 2 0 002-2V6a2 2 0 00-2-2H6a2 2 0 00-2 2v12a2 2 0 002 2z" />
                     </svg>
                 </div>
 
                 {/* Preset + */}
                 <div
-                    className="w-full h-8 border rounded border-gray-400 flex items-center justify-center text-gray-400
+                    className="w-full h-8 border rounded border-gray-400 flex items-center justify-center text-gray-500
                         transform active:scale-[.97] cursor-pointer"
                     title="Save preset"
                     onClick={appendNew}
                 >
                     <svg className="h-6 w-6" fill="none" viewBox="0 0 24 24" stroke="currentColor">
-                        <path strokeLinecap="round" strokeLinejoin="round" strokeWidth={.6} d="M17 14v6m-3-3h6M6 10h2a2 2 0 002-2V6a2 2 0 00-2-2H6a2 2 0 00-2 2v2a2 2 0 002 2zm10 0h2a2 2 0 002-2V6a2 2 0 00-2-2h-2a2 2 0 00-2 2v2a2 2 0 002 2zM6 20h2a2 2 0 002-2v-2a2 2 0 00-2-2H6a2 2 0 00-2 2v2a2 2 0 002 2z" />
+                        <path strokeLinecap="round" strokeLinejoin="round" strokeWidth={.8} d="M17 14v6m-3-3h6M6 10h2a2 2 0 002-2V6a2 2 0 00-2-2H6a2 2 0 00-2 2v2a2 2 0 002 2zm10 0h2a2 2 0 002-2V6a2 2 0 00-2-2h-2a2 2 0 00-2 2v2a2 2 0 002 2zM6 20h2a2 2 0 002-2v-2a2 2 0 00-2-2H6a2 2 0 00-2 2v2a2 2 0 002 2z" />
                     </svg>
                 </div>
 
                 {/* Preset save */}
                 <div
-                    className="w-full h-8 border rounded border-gray-400 flex items-center justify-center text-gray-400
+                    className="w-full h-8 border rounded border-gray-400 flex items-center justify-center text-gray-500
                         transform active:scale-[.97] cursor-pointer"
                     title="Save image"
                     onClick={(event) => saveItemPng(event)}
                 >
                     <svg className="h-6 w-6" fill="none" viewBox="0 0 24 24" stroke="currentColor">
-                        <path strokeLinecap="round" strokeLinejoin="round" strokeWidth={.6} d="M8 7H5a2 2 0 00-2 2v9a2 2 0 002 2h14a2 2 0 002-2V9a2 2 0 00-2-2h-3m-1 4l-3 3m0 0l-3-3m3 3V4" />
+                        <path strokeLinecap="round" strokeLinejoin="round" strokeWidth={.8} d="M8 7H5a2 2 0 00-2 2v9a2 2 0 002 2h14a2 2 0 002-2V9a2 2 0 00-2-2h-3m-1 4l-3 3m0 0l-3-3m3 3V4" />
                     </svg>
                 </div>
             </div>
