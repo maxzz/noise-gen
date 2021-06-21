3884d9b1709c8a4927fbbd62f311f5202c676db4
Max Zakharzhevskiy <maxzz@msn.com>
2021-06-21 00:42:50 -0700

    WIP: storage format. 8. add own atomWithLocalStorage. 3. generic version

--- src/hooks/useLocalStorage.ts
+++ src/hooks/useLocalStorage.ts
@@ -1,6 +1,20 @@
 import { atom, WritableAtom } from 'jotai';
 
-export function atomWithLocalStorage<Value>(key: string, initialValue: Value): WritableAtom<Value, Value> {
+export function atomWithCallback<Value>(initialValue: Value, onValueChange: (nextValue: Value) => void): WritableAtom<Value, Value> {
+    const baseAtom = atom(initialValue);
+    const derivedAtom = atom<Value, Value>(
+        (get) => get(baseAtom),
+        (get, set, update) => {
+            const nextValue = typeof update === 'function' ? update(get(baseAtom)) : update;
+            set(baseAtom, nextValue);
+            onValueChange(nextValue);
+        }
+    );
+    return derivedAtom;
+}
+
+/*
+export function atomWithLocalStorage<Value>(key: string, initialValue: Value, onValueChange: (nextValue: Value) => void): WritableAtom<Value, Value> {
     const baseAtom = atom(initialValue);
     const derivedAtom = atom<Value, Value>(
         (get) => get(baseAtom),
@@ -13,3 +27,4 @@ export function atomWithLocalStorage<Value>(key: string, initialValue: Value): W
     );
     return derivedAtom;
 }
+*/
