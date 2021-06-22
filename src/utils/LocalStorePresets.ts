f0db6651d57be56469b61f7c64909259dd657014
Max Zakharzhevskiy <maxzz@msn.com>
2021-06-21 19:19:32 -0700

    Add packing preset params

--- src/utils/LocalStorePresets.ts
+++ src/utils/LocalStorePresets.ts
@@ -1,13 +1,13 @@
 import { Getter } from 'jotai';
 import { PresetsAtom } from '../atoms';
 import debounce from './debounce';
-import { PresetData, RenderParams, STORAGE_KEY } from './types';
+import { PresetData, renderParams2Store, renderParams4Store, STORAGE_KEY } from './types';
 
 const PRESETS_KEY = `${STORAGE_KEY}-presets2`;
 
 type PresetsStoreItem = {
     id: string;
-    rpm: RenderParams;
+    rpm: string;
 };
 
 type PresetsStoreCache = Record<string, string>; // Item ID -> data:string URL
@@ -19,12 +19,15 @@ export function defPresets(): PresetData[] {
         let dataRaw = setsRaw && JSON.parse(setsRaw) as PresetsStoreItem[] || [];
         let cache = cacheRaw && JSON.parse(cacheRaw) as PresetsStoreCache || {};
 
-        let data = dataRaw.map((preset: PresetsStoreItem): PresetData => ({
-            id: preset.id,
-            preview: cache[preset.id],
-            renderParams: preset.rpm,
-        }));
-        
+        let data = dataRaw.map((preset: PresetsStoreItem): PresetData | undefined => {
+            let renderParams = renderParams4Store(preset.rpm);
+            return !renderParams || !preset.id ? undefined : {
+                id: preset.id,
+                preview: cache[preset.id],
+                renderParams: renderParams,
+            };
+        }).filter(Boolean) as PresetData[];
+
         return data;
         //let rpm = renderParams4Store(data.rpm);
         // if (rpm) {
@@ -45,7 +48,7 @@ export const storePresets = debounce((get: Getter) => {
     let cache: PresetsStoreCache = {};
     let data = dataRaw.map((preset: PresetData): PresetsStoreItem => {
         preset.preview && (cache[preset.id] = preset.preview);
-        return { id: preset.id, rpm: preset.renderParams };
+        return { id: preset.id, rpm: renderParams2Store(preset.renderParams) };
     });
 
     localStorage.setItem(`${PRESETS_KEY}-sets`, JSON.stringify(data));
