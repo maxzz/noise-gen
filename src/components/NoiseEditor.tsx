26974c6cf5a967c985eaadd43d9c84eb910fa57b
Max Zakharzhevskiy <maxzz@msn.com>
2021-06-23 15:39:31 -0700

    WIP. noise UI. 13. add special sliders for noise editor

--- src/components/NoiseEditor.tsx
+++ src/components/NoiseEditor.tsx
@@ -1,16 +1,32 @@
 import React from 'react';
-import Slider from './Slider';
+import { SliderProps } from './Slider';
 import ChevronHorizontal from './ChevronHorizontal';
 
 const noises = ['2D', '3D', '4D'];
 
-function NoiseTypekBox({ text, selected, onClick }: { text: string; selected: boolean; onClick: () => void; }) {
+function NoiseTypeBox({ text, selected, onClick }: { text: string; selected: boolean; onClick: () => void; }) {
     return (
         <div className={`w-5 h-5 px-1 py-0.5 border border-purple-400 rounded flex items-center justify-center cursor-pointer
             ${selected ? 'bg-white ring-1 ring-offset-1 ring-offset-red-100 ring-purple-400' : ''}`}
             onClick={onClick}
         >{text}</div>
 
+    );
+}
+
+function Slider({ label, min, max, step = .01, labelWidth = '4.5rem', value, onChange}: SliderProps) {
+    return (
+        <div className="px-2 w-full h-4 flex items-center justify-center space-x-2 text-xs text-purple-900">
+            <div className="flex-none" style={{ width: labelWidth }}>{label}</div>
+            <input
+                className="ui-slider" type="range"
+                value={value} onChange={(event) => onChange(+event.target.value)}
+                min={min} max={max} step={step}
+            />
+            <input className="w-8 bg-purple-100 text-[.6rem]"
+                value={value} onChange={(event) => onChange(+event.target.value)}
+            />
+        </div>
     );
 }
 
@@ -37,14 +53,14 @@ function NoiseEditor() {
                 <div className="flex items-center text-xs select-none">
                     <div className="">Noise</div>
                     <div className="pl-2 flex items-center text-[.6rem] space-x-1">
-                        <NoiseTypekBox text="2D" selected={selected === 0} onClick={() => setNoise(0)} />
-                        <NoiseTypekBox text="3D" selected={selected === 1} onClick={() => setNoise(1)} />
-                        <NoiseTypekBox text="4D" selected={selected === 2} onClick={() => setNoise(2)} />
+                        <NoiseTypeBox text="2D" selected={selected === 0} onClick={() => setNoise(0)} />
+                        <NoiseTypeBox text="3D" selected={selected === 1} onClick={() => setNoise(1)} />
+                        <NoiseTypeBox text="4D" selected={selected === 2} onClick={() => setNoise(2)} />
                     </div>
                 </div>
 
                 {/* Noise params */}
-                <div className="pl-1 text-right">
+                <div className="pl-1 mt-1 text-right">
                     <Slider labelWidth="2rem" min={1} max={10} value={2} onChange={setScale} label="x" />
                     <Slider labelWidth="2rem" min={1} max={10} value={2} onChange={setScale} label="y" />
                     <Slider labelWidth="2rem" min={1} max={10} value={2} onChange={setScale} label="z" />
--- src/components/Slider.tsx
+++ src/components/Slider.tsx
@@ -11,15 +11,7 @@ export interface SliderProps {
     onChange: (value: number) => void;
 }
 
-function Slider({
-    label,
-    min,
-    max,
-    step = .01,
-    labelWidth = '4.5rem',
-    value,
-    onChange
-}: SliderProps) {
+function Slider({ label, min, max, step = .01, labelWidth = '4.5rem', value, onChange}: SliderProps) {
     return (
         <div className="px-2 w-full h-5 flex items-center justify-center space-x-2 text-xs text-purple-900">
             <div className="flex-none" style={{ width: labelWidth }}>{label}</div>
