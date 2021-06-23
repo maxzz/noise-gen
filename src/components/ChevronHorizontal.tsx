3737f8359e6367330912e75f270d5cde436f57ff
Max Zakharzhevskiy <maxzz@msn.com>
2021-06-23 15:00:21 -0700

    WIP. noise UI. 8. new noise choose controls

--- src/components/ChevronHorizontal.tsx
+++ src/components/ChevronHorizontal.tsx
@@ -2,7 +2,7 @@ import React from 'react';
 
 function ChevronHorizontal() {
     return (
-        <svg className="h-5 w-5 p-1.5" fill="none" viewBox="0 0 24 24" stroke="currentColor">
+        <svg className="h-4 w-4" fill="none" viewBox="0 0 24 24" stroke="currentColor">
             <path strokeLinecap="round" strokeLinejoin="round" strokeWidth={2} d="M11 19l-7-7 7-7m8 14l-7-7 7-7" />
         </svg>
     );
--- src/components/NoiseEditor.tsx
+++ src/components/NoiseEditor.tsx
@@ -17,44 +17,56 @@ function NoiseEditor() {
     }
 
     return (
-        <div className="w-36 -mt-2 pt-2 flex flex-col">
-            {/* Noise type */}
-            <div className="relative flex items-center">
-
-                <div className="text-xs">Noise</div>
-                
-                <RadioGroup value={selected} onChange={setNoise}>
-                    <div className="px-2 flex place-content-evenly space-x-1">
-                        {noises.map((noise, index) => (
-                            <RadioGroup.Option
-                                key={index}
-                                value={index}
-                                className={
-                                    ({ active, checked }) => `
-                                            ${active ? 'ring-1 ring-offset-1 ring-offset-sky-300 ring-white ring-opacity-60' : ''}
-                                            ${checked ? 'bg-sky-900 bg-white' : 'bg-purple-100'}
-                                            w-5 h-5 relative text-[.6rem] rounded-[4px] px-1 py-0.5 cursor-pointer flex focus:outline-none
-                                            border border-gray-500
-                                            `
-                                }
-                            >
-                                {noises[index]}
-                            </RadioGroup.Option>
-                        ))}
-                    </div>
-                </RadioGroup>
-
-                <div className="absolute -top-0.5 right-1 p-1 cursor-pointer hover:ring-1 ring-gray-600 rounded-[4px] scale-[1]">
-                    <ChevronHorizontal />
-                </div>
+        <div className="w-36 -mt-2 pt-2 pl-1 relative border-l border-purple-300 flex flex-col">
+            <div className="absolute top-1.5 right-1 p-1 cursor-pointer hover:ring-1 ring-gray-600 rounded-[4px] scale-[1]">
+                <ChevronHorizontal />
             </div>
 
-            {/* Noise params */}
-            <div className="pl-1 text-right">
-                <Slider labelWidth="2rem" min={1} max={10} value={2} onChange={setScale} label="x" />
-                <Slider labelWidth="2rem" min={1} max={10} value={2} onChange={setScale} label="y" />
-                <Slider labelWidth="2rem" min={1} max={10} value={2} onChange={setScale} label="z" />
-                <Slider labelWidth="2rem" min={1} max={10} value={2} onChange={setScale} label="w" />
+            {/* Editor body */}
+            <div className="">
+                <div className="flex items-center text-xs">
+                    <div className="">Noise</div>
+                    <div className="pl-2 flex items-center text-[.6rem] space-x-1">
+                        <div className={`w-5 h-5 px-1 py-0.5 border border-purple-400 rounded flex items-center justify-center cursor-pointer
+                            ${selected === 0 ? 'bg-white': ''}`}>2D</div>
+                        <div className={`w-5 h-5 px-1 py-0.5 border border-purple-400 rounded flex items-center justify-center cursor-pointer
+                            ${selected === 1 ? 'bg-white': ''}`}>3D</div>
+                        <div className={`w-5 h-5 px-1 py-0.5 border border-purple-400 rounded flex items-center justify-center cursor-pointer
+                            ${selected === 2 ? 'bg-white': ''}`}>4D</div>
+                    </div>
+                </div>
+
+                {/* Noise type */}
+                <div className="flex items-center mt-2">
+                    <div className="text-xs">Noise</div>
+                    <RadioGroup value={selected} onChange={setNoise}>
+                        <div className="px-2 flex place-content-evenly space-x-1">
+                            {noises.map((noise, index) => (
+                                <RadioGroup.Option
+                                    key={index}
+                                    value={index}
+                                    className={
+                                        ({ active, checked }) => `
+                                                ${active ? 'ring-1 ring-offset-1 ring-offset-sky-300 ring-white ring-opacity-60' : ''}
+                                                ${checked ? 'bg-sky-900 bg-white' : 'bg-purple-100'}
+                                                w-5 h-5 relative text-[.6rem] rounded-[4px] px-1 py-0.5 cursor-pointer flex focus:outline-none
+                                                border border-gray-500
+                                                `
+                                    }
+                                >
+                                    {noises[index]}
+                                </RadioGroup.Option>
+                            ))}
+                        </div>
+                    </RadioGroup>
+                </div>
+                {/* Noise params */}
+                <div className="pl-1 text-right">
+                    <Slider labelWidth="2rem" min={1} max={10} value={2} onChange={setScale} label="x" />
+                    <Slider labelWidth="2rem" min={1} max={10} value={2} onChange={setScale} label="y" />
+                    <Slider labelWidth="2rem" min={1} max={10} value={2} onChange={setScale} label="z" />
+                    <Slider labelWidth="2rem" min={1} max={10} value={2} onChange={setScale} label="w" />
+                </div>
             </div>
         </div>
     );
