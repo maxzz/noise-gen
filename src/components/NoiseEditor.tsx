89c9d50134c333dcf96e59c32ec29da1e5b502b7
Max Zakharzhevskiy <maxzz@msn.com>
2021-06-23 15:29:32 -0700

    Code clean up. remove @headlessui/react

--- src/components/NoiseEditor.tsx
+++ src/components/NoiseEditor.tsx
@@ -1,5 +1,4 @@
 import React from 'react';
-//import { RadioGroup } from '@headlessui/react';
 import Slider from './Slider';
 import ChevronHorizontal from './ChevronHorizontal';
 
@@ -34,52 +33,16 @@ function NoiseEditor() {
 
             {/* Editor body */}
             <div className="">
+                {/* Noise type */}
                 <div className="flex items-center text-xs select-none">
                     <div className="">Noise</div>
                     <div className="pl-2 flex items-center text-[.6rem] space-x-1">
                         <NoiseTypekBox text="2D" selected={selected === 0} onClick={() => setNoise(0)} />
                         <NoiseTypekBox text="3D" selected={selected === 1} onClick={() => setNoise(1)} />
                         <NoiseTypekBox text="4D" selected={selected === 2} onClick={() => setNoise(2)} />
-
-                        {/* <div className={`w-5 h-5 px-1 py-0.5 border border-purple-400 rounded flex items-center justify-center cursor-pointer
-                            ${selected === 0 ? 'bg-white ring-1 ring-offset-1 ring-offset-red-100 ring-purple-400' : ''}`}
-                            onClick={() => setNoise(0)}
-                        >2D</div>
-                        <div className={`w-5 h-5 px-1 py-0.5 border border-purple-400 rounded flex items-center justify-center cursor-pointer
-                            ${selected === 1 ? 'bg-white ring-1 ring-offset-1 ring-offset-red-100 ring-purple-400' : ''}`}
-                            onClick={() => setNoise(1)}
-                        >3D</div>
-                        <div className={`w-5 h-5 px-1 py-0.5 border border-purple-400 rounded flex items-center justify-center cursor-pointer
-                            ${selected === 2 ? 'bg-white ring-1 ring-offset-1 ring-offset-red-100 ring-purple-400' : ''}`}
-                            onClick={() => setNoise(2)}
-                        >4D</div> */}
                     </div>
                 </div>
 
-                {/* Noise type */}
-                {/* <div className="flex items-center mt-2">
-                    <div className="text-xs">Noise</div>
-                    <RadioGroup value={selected} onChange={setNoise}>
-                        <div className="px-2 flex place-content-evenly space-x-1">
-                            {noises.map((noise, index) => (
-                                <RadioGroup.Option
-                                    key={index}
-                                    value={index}
-                                    className={
-                                        ({ active, checked }) => `
-                                                ${active ? 'ring-1 ring-offset-1 ring-offset-sky-300 ring-white ring-opacity-60' : ''}
-                                                ${checked ? 'bg-sky-900 bg-white' : 'bg-purple-100'}
-                                                w-5 h-5 relative text-[.6rem] rounded-[4px] px-1 py-0.5 cursor-pointer flex focus:outline-none
-                                                border border-gray-500
-                                                `
-                                    }
-                                >
-                                    {noises[index]}
-                                </RadioGroup.Option>
-                            ))}
-                        </div>
-                    </RadioGroup>
-                </div> */}
                 {/* Noise params */}
                 <div className="pl-1 text-right">
                     <Slider labelWidth="2rem" min={1} max={10} value={2} onChange={setScale} label="x" />
