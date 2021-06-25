d2bf9454b30879542c711845f830577065424ca7
Max Zakharzhevskiy <maxzz@msn.com>
2021-06-24 17:59:39 -0700

    Fix: ButtonShowNoise re-work

--- src/components/ButtonShowNoise.tsx
+++ src/components/ButtonShowNoise.tsx
@@ -11,9 +11,9 @@ function ButtonShowNoise() {
             title="Show/Hide noise parameters"
             onClick={() => setShowNoiseEditor(prev => !prev)}
         >
-            <svg className="" viewBox="0 0 101 101" fill="none" stroke="currentColor" strokeWidth={3}>
+            <svg className="" viewBox="0 0 101 101" fill="none" stroke="currentColor" strokeWidth={4}>
                 <path d="M88.3 139c11.6-34 23.8-35.6 35 0 10.5 33.2 23.4 34 35 0" transform="translate(-72.8 -88)" />
-                <path d="M15.4 32.4v35.7M12.1 50.5h78.8" />
+                <path d="M15.4 22.4v55.7M12.1 50.5h78.8" strokeWidth={2} />
             </svg>
         </div>
     );
