738c130f330d0fa83813e9a08ba14fab38236400
Max Zakharzhevskiy <maxzz@msn.com>
2021-06-24 17:22:14 -0700

    Fix: ButtonChooseColorBg re-work

--- src/components/ButtonChooseColorBg.tsx
+++ src/components/ButtonChooseColorBg.tsx
@@ -2,34 +2,37 @@ import { useAtom } from 'jotai';
 import React from 'react';
 import { ColorCanvasAtom } from '../atoms';
 
-const bgColors = ['black', 'white', 'transparent'];
-const bgTitle = `Perview canvas background: white/black/transparent.
+const BGCOLORS = ['black', 'white', 'transparent'];
+
+const hint = `Perview canvas background: white/black/transparent.
 The saved image will have a transparent background.`;
 
 function ButtonChooseColorBg() {
     const [backgorund, setBackgorund] = useAtom(ColorCanvasAtom);
-    function changeBackground() {
-        let newColor = (bgColors.findIndex((item) => item === backgorund) + 1) % bgColors.length;
-        setBackgorund(bgColors[newColor]);
+
+    function changeBkg() {
+        let idx = (BGCOLORS.findIndex((item) => item === backgorund) + 1) % BGCOLORS.length;
+        setBackgorund(BGCOLORS[idx]);
     }
+
     return (
-        <div
-            className="w-8 h-8 rounded dark-frame active-scale relative cursor-pointer"
+        <button
+            className="w-8 h-8 relative rounded dark-frame no-active-ouline active-scale"
             style={{borderWidth: backgorund === 'transparent' ? 1 : 0}}
-            title={bgTitle}
-            onClick={changeBackground}
+            title={hint}
+            onClick={changeBkg}
         >
-            <div className="absolute border dark-frame rounded-[0.15rem] inset-[2px]"></div>
+            {/* <div className="absolute border dark-frame rounded-[0.15rem] inset-[2px]"></div> */}
 
-            <div className="py-2 h-full flex items-center justify-evenly border-2 dark-frame rounded"
-
+            <div
+                className="py-2 h-full flex items-center justify-evenly border-2 dark-frame rounded"
                 style={{borderColor: backgorund}}
             >
                 <div className="w-1.5 h-1/2 bg-black"></div>
                 <div className="w-1.5 h-1/2 bg-white"></div>
                 <div className="w-1.5 h-1/2 border dark-frame"></div>
             </div>
-        </div>
+        </button>
     );
 }
 
