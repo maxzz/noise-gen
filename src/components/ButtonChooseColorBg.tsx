fcc1d0b5559b4e2b889721009edb023ceb7f70da
Max Zakharzhevskiy <maxzz@msn.com>
2021-06-24 17:54:44 -0700

    Code clean up.

--- src/components/ButtonChooseColorBg.tsx
+++ src/components/ButtonChooseColorBg.tsx
@@ -27,10 +27,6 @@ function ButtonChooseColorBg() {
                 <div className={`${backgorund === 'black' ? 'w-1/2 h-2/3' : 'w-1/3 h-1'} bg-black`}></div>
                 <div className={`${backgorund === 'white' ? 'w-1/2 h-2/3' : 'w-1/3 h-1'} bg-white`}></div>
                 <div className={`${backgorund === 'transparent' ? 'w-1/2 h-2/3' : 'w-1/3 h-1'} border dark-frame`}></div>
-
-                {/* <div className={`w-1/3 ${backgorund === 'black' ? 'h-2/3' : 'h-1'} bg-black`}></div>
-                <div className={`w-1/3 ${backgorund === 'white' ? 'h-2/3' : 'h-1'} bg-white`}></div>
-                <div className={`w-1/3 ${backgorund === 'transparent' ? 'h-2/3' : 'h-1'} border dark-frame`}></div> */}
             </div>
         </button>
     );
