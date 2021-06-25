b29639f515186f5bf389b7ab4d3939e502216ed9
Max Zakharzhevskiy <maxzz@msn.com>
2021-06-24 23:29:33 -0700

    The noise-panel appearance better animation

--- src/components/NoiseEditor.tsx
+++ src/components/NoiseEditor.tsx
@@ -24,7 +24,8 @@ function NoiseEditor() {
     const [, setNoiseScale] = useAtom(SetNoiseScaleAtom);
 
     const props = useSpring({
-        width: showNoiseEditor ? 144 : 0
+        opacity: showNoiseEditor ? 1 : 0,
+        width: showNoiseEditor ? 144 : 0,
     });
 
     function setNoise(value: number) {
@@ -39,24 +40,26 @@ function NoiseEditor() {
         <div className="relative text-purple-900 border-l border-purple-300 flex flex-col">
             {/* Editor body */}
             {/* {showNoiseEditor && */}
-                <a.div className="-mt-2 pt-2 pl-1 overflow-hidden" style={{ width: props.width }}>
+                <a.div className="-mt-2 pt-2 pl-1 overflow-hidden" style={{ width: props.width, opacity: props.opacity }}>
                     {/* <div className="w-36 -mt-2 pt-2 pl-1"> */}
-                    {/* Noise type */}
-                    <div className="flex items-center text-xs select-none">
-                        <div className="">Noise</div>
-                        <div className="pl-2 flex items-center text-[.6rem] space-x-1">
-                            <NoiseTypeBox text="2D" selected={noise.dim === 2} onClick={() => setNoise(2)} />
-                            <NoiseTypeBox text="3D" selected={noise.dim === 3} onClick={() => setNoise(3)} />
-                            <NoiseTypeBox text="4D" selected={noise.dim === 4} onClick={() => setNoise(4)} />
-                        </div>
-                    </div>
 
-                    {/* Noise params */}
-                    <div className="pl-1 mt-1 text-right">
-                        <SmallSlider labelWidth="2rem" min={NOISEPARAMS.d3.min.x} max={NOISEPARAMS.d3.max.x} value={noise.x} onChange={(value) => setScale('x', value)} label="scale x" />
-                        <SmallSlider labelWidth="2rem" min={NOISEPARAMS.d3.min.y} max={NOISEPARAMS.d3.max.y} value={noise.y} onChange={(value) => setScale('y', value)} label="scale y" />
-                        {noise.dim > 2 && <SmallSlider labelWidth="2rem" min={NOISEPARAMS.d3.min.z} max={NOISEPARAMS.d3.max.z} value={noise.z} onChange={(value) => setScale('z', value)} label="scale z" />}
-                        {noise.dim > 3 && <SmallSlider labelWidth="2rem" min={NOISEPARAMS.d3.min.w} max={NOISEPARAMS.d3.max.w} value={noise.w} onChange={(value) => setScale('w', value)} label="scale w" />}
+                    <div className="w-36">
+                        {/* Noise type */}
+                        <div className="flex items-center text-xs select-none">
+                            <div className="">Noise</div>
+                            <div className="pl-2 flex items-center text-[.6rem] space-x-1">
+                                <NoiseTypeBox text="2D" selected={noise.dim === 2} onClick={() => setNoise(2)} />
+                                <NoiseTypeBox text="3D" selected={noise.dim === 3} onClick={() => setNoise(3)} />
+                                <NoiseTypeBox text="4D" selected={noise.dim === 4} onClick={() => setNoise(4)} />
+                            </div>
+                        </div>
+                        {/* Noise params */}
+                        <div className="pl-1 mt-1 text-right">
+                            <SmallSlider labelWidth="2rem" min={NOISEPARAMS.d3.min.x} max={NOISEPARAMS.d3.max.x} value={noise.x} onChange={(value) => setScale('x', value)} label="scale x" />
+                            <SmallSlider labelWidth="2rem" min={NOISEPARAMS.d3.min.y} max={NOISEPARAMS.d3.max.y} value={noise.y} onChange={(value) => setScale('y', value)} label="scale y" />
+                            {noise.dim > 2 && <SmallSlider labelWidth="2rem" min={NOISEPARAMS.d3.min.z} max={NOISEPARAMS.d3.max.z} value={noise.z} onChange={(value) => setScale('z', value)} label="scale z" />}
+                            {noise.dim > 3 && <SmallSlider labelWidth="2rem" min={NOISEPARAMS.d3.min.w} max={NOISEPARAMS.d3.max.w} value={noise.w} onChange={(value) => setScale('w', value)} label="scale w" />}
+                        </div>
                     </div>
                 </a.div>
             {/* } */}
