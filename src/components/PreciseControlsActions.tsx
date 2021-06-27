82ac279af53e40b7be1f9f3b43e08ad8248ad37b
Max Zakharzhevskiy <maxzz@msn.com>
2021-06-26 23:28:55 -0700

    WIP: add save image size popup. 1.

--- src/components/PreciseControlsActions.tsx
+++ src/components/PreciseControlsActions.tsx
@@ -13,19 +13,26 @@ function PreciseControlsActions() {
         worker?.postMessage({ type: 'get-preview', smallWidth: PRESET_W, smallHeight: PRESET_H } as I2W.GetPreview);
     }
 
-    async function saveItemPng(event: React.MouseEvent) {
-        if (worker) {
-            let blob = await worker.getImage();
-            saveBlobData(blob, 'noise-gen.png');
-        }
-    }
-
     async function setAsBackground(event: React.MouseEvent) {
         if (worker) {
             let blob = await worker.getImage();
             setAppBackgroundUrl(blob);
         }
     }
+
+    // async function saveItemPng(event: React.MouseEvent) {
+    //     if (worker) {
+    //         let blob = await worker.getImage();
+    //         saveBlobData(blob, 'noise-gen.png');
+    //     }
+    // }
+
+    const showSelectFileSize = React.useState(true);
+
+    async function saveItemPng(event: React.MouseEvent) {
+
+    }
+
     return (
         <div className="p-2 flex space-x-1">
 
@@ -60,14 +67,27 @@ function PreciseControlsActions() {
 
             {/* Preset save */}
             <div
-                className="flex-1 w-8 h-8 border rounded border-gray-400 flex-centered text-gray-500
-                transform active-scale cursor-pointer"
+                className="relative flex-1 w-8 h-8 border rounded border-gray-400 flex-centered text-gray-500
+                transform active-scale cursor-pointer z-10"
                 title="Save image"
                 onClick={(event) => saveItemPng(event)}
             >
                 <svg className="h-6 w-6" fill="none" viewBox="0 0 24 24" stroke="currentColor">
                     <path strokeLinecap="round" strokeLinejoin="round" strokeWidth={.8} d="M8 7H5a2 2 0 00-2 2v9a2 2 0 002 2h14a2 2 0 002-2V9a2 2 0 00-2-2h-3m-1 4l-3 3m0 0l-3-3m3 3V4" />
                 </svg>
+
+                {showSelectFileSize &&
+                    <div className="absolute mt-1 px-2 top-full right-0 rounded border border-gray-400 bg-purple-300 flex-col shadow">
+                        <div className="">Size</div>
+                        <div className="flex items-center space-x-1">
+                            
+                            <input className="px-2 py-0.5 w-16 rounded" />
+                            <div className="">x</div>
+                            <input className="px-2 py-0.5 w-16 rounded" />
+                        </div>
+                        <button className="">Save</button>
+                    </div>
+                }
             </div>
         </div>
     );
