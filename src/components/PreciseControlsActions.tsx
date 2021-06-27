1c859a6c7bac4adc792fa205efeb3649d47eeccb
Max Zakharzhevskiy <maxzz@msn.com>
2021-06-26 23:47:11 -0700

    WIP: add save image size popup. 5. popup as component

--- src/components/PreciseControlsActions.tsx
+++ src/components/PreciseControlsActions.tsx
@@ -5,6 +5,26 @@ import { I2W, PRESET_H, PRESET_W } from '../utils/types';
 import SizeBoxes from './SizeBoxes';
 import saveBlobData from '../utils/saveImage';
 
+function DimentionsPopup() {
+    return (
+        <div className="px-2 py-1 rounded border text-sm border-gray-400 bg-purple-300 flex flex-col shadow">
+            <div className="">Image size</div>
+            <div className="mt-1 flex items-center space-x-1">
+                <input className="px-2 py-0.5 w-16 rounded" />
+                <div className="">
+                    <svg className="h-5 w-5" fill="none" viewBox="0 0 24 24" stroke="currentColor">
+                        <path strokeLinecap="round" strokeLinejoin="round" strokeWidth={2} d="M6 18L18 6M6 6l12 12" />
+                    </svg>
+                </div>
+                <input className="px-2 py-0.5 w-16 rounded" />
+            </div>
+            <button className="self-end mt-2 px-2 py-1 rounded border border-gray-200 text-gray-200 active-scale">
+                Save
+            </button>
+        </div>
+    );
+}
+
 function PreciseControlsActions() {
     const [worker] = useAtom(RenderWorkerAtom);
     const [, setAppBackgroundUrl] = useAtom(AppBackgroundUrlAtom);
@@ -77,20 +97,8 @@ function PreciseControlsActions() {
                 </svg>
 
                 {showSelectFileSize &&
-                    <div className="absolute mt-1 px-2 py-1 top-full right-0 rounded border text-sm border-gray-400 bg-purple-300 flex flex-col shadow">
-                        <div className="">Image size</div>
-                        <div className="mt-1 flex items-center space-x-1">
-                            <input className="px-2 py-0.5 w-16 rounded" />
-                            <div className="">
-                                <svg className="h-5 w-5" fill="none" viewBox="0 0 24 24" stroke="currentColor">
-                                    <path strokeLinecap="round" strokeLinejoin="round" strokeWidth={2} d="M6 18L18 6M6 6l12 12" />
-                                </svg>
-                            </div>
-                            <input className="px-2 py-0.5 w-16 rounded" />
-                        </div>
-                        <button className="self-end mt-2 px-2 py-1 rounded border border-gray-200 text-gray-200 active-scale">
-                            Save
-                        </button>
+                    <div className="absolute mt-1 top-full right-0">
+                        <DimentionsPopup />
                     </div>
                 }
             </div>
