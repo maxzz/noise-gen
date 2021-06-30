193864c31e4d2c6e4857598fe05a603b9de51f8d
Max Zakharzhevskiy <maxzz@msn.com>
2021-06-30 00:29:01 -0700

    WIP: input numbers validation. 11. rounding problem

--- src/hooks/useFloatInput.ts
+++ src/hooks/useFloatInput.ts
@@ -16,7 +16,18 @@ export default function useFloatInput(value: number, range: InputRange, onChange
 
     React.useEffect(() => {
         setLocal('' + value);
+        console.log('input useEffect', value);
     }, [value]);
+
+    function setLocalValue(s: string): void {
+        console.log('setLocalValue', s);
+
+        setLocal(s);
+        let n = +beautifyFloat(s);
+        if (!isNaN(n)) {
+            onChange(n);
+        }
+    }
 
     const onSliderChange = (event: React.ChangeEvent<HTMLInputElement>) => {
         setLocal(event.target.value);
@@ -24,11 +35,7 @@ export default function useFloatInput(value: number, range: InputRange, onChange
     };
 
     const onInputChange = (event: React.ChangeEvent<HTMLInputElement>) => {
-        setLocal(event.target.value);
-        let n = +beautifyFloat(event.target.value);
-        if (!isNaN(n)) {
-            onChange(n);
-        }
+        setLocalValue(event.target.value);
     };
 
     const onSliderKey = (event: React.KeyboardEvent) => {
@@ -39,8 +46,30 @@ export default function useFloatInput(value: number, range: InputRange, onChange
         if (!isNaN(n)) {
             let shift = getShift4Input(range.step, event);
             if (shift) {
-                let newN = n + shift;
-                setLocal('' + constrainRange(newN, range.min, range.max));
+                let newN = (n * 10000 + shift * 10000) / 10000;
+
+                let stepfraction = fractionLength(range.step);
+                let shiftfraction = fractionLength(newN);
+
+                console.log('local', local, 'n', n, 'newN', newN, 'shift', shift, 'step', range.step);
+
+                // -0.09 + -0.01 = -0.09999999999999999
+                // 0.07 + -0.01 = 0.060000000000000005
+                // (0.07 * 10000  + -0.01 * 10000) / 10000 = 0.06000000000000001
+
+                // let stepfraction = fractionLength(range.step);
+                // let shiftfraction = fractionLength(newN);
+                // let cut = Math.max(stepfraction, shiftfraction);
+
+                // console.log('step', range.step, 'shift', shift, 'shiftFraction', shiftfraction, 'value str', local, 'value num', n);
+
+                // if (shiftfraction > stepfraction) {
+                //     newN = +local.replace(/0$/, '');
+                //     console.log('set n', newN);
+                // }
+    
+                //setLocal('' + constrainRange(newN, range.min, range.max));
+                setLocalValue('' + constrainRange(newN, range.min, range.max));
             }
         }
     };
