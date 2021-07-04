0695f086693df1835117056c1c0701a936ac42fd
Max Zakharzhevskiy <maxzz@msn.com>
2021-07-04 16:01:22 -0700

    WIP Cube animation. working numbers

--- src/components/TestCubeAnimation.tsx
+++ src/components/TestCubeAnimation.tsx
@@ -1,7 +1,7 @@
 import React from 'react';
 
 const FACES = [
-    [0],
+    [4],
     [0, 8],
     [0, 4, 8],
     [0, 2, 6, 8],
@@ -9,25 +9,27 @@ const FACES = [
     [0, 2, 3, 5, 6, 8],
 ];
 
-function Dot() {
-    
+function Dot({on}: {on: boolean}) {
+    return (
+        <div className={`w-full h-full rounded-full ${on ? 'bg-gray-700' : 'bg-transparent'}`}></div>
+    );
 }
 
 function CubeFace() {
-    const [digit, setDigit] = React.useState(0);
+    const [digit, setDigit] = React.useState(6);
+
+    let items = [];
+    let faces = FACES[digit - 1];
+
+    if (faces) {
+        for (let i = 0; i < 9; i++) {
+            items.push(<Dot on={faces.includes(i)} key={i} />);
+        }
+    }
+
     return (
         <div className="p-4 bg-red-400 w-32 h-32 grid grid-cols-3 grid-rows-3 gap-2">
-            <div className="w-full h-full rounded-full bg-gray-700"></div>
-            <div className="w-full h-full rounded-full bg-transparent"></div>
-            <div className="w-full h-full rounded-full bg-gray-700"></div>
-
-            <div className="w-full h-full rounded-full bg-gray-700"></div>
-            <div className="w-full h-full rounded-full bg-gray-700"></div>
-            <div className="w-full h-full rounded-full bg-gray-700"></div>
-
-            <div className="w-full h-full rounded-full bg-gray-700"></div>
-            <div className="w-full h-full rounded-full bg-transparent"></div>
-            <div className="w-full h-full rounded-full bg-gray-700"></div>
+            {items.map((item: React.ReactNode) => item)}
         </div>
     );
 }
