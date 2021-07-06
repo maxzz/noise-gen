a25e8306028803fd4e8a9b53bb7cc5ca1972965f
Max Zakharzhevskiy <maxzz@msn.com>
2021-07-05 17:53:58 -0700

    WIP Cube animation. remove items generation in old fashion

--- src/components/TestCubeAnimation.tsx
+++ src/components/TestCubeAnimation.tsx
@@ -25,14 +25,12 @@ function Face({ digit }: { digit: number; }) {
         return items;
     }, [digit]);
 
-    const [styles, api] = useSpring(() => ({
-        rotate: 0,
-        scale: .5,
-        backgroundColor: 'rgb(167, 139, 250)',
-    }));
+    // const [styles, api] = useSpring(() => ({
+    //     //scale: .5,
+    // }));
 
     return (
-        <a.div style={styles} className="rounded-lg ring-2 ring-gray-300 bg-purple-400">
+        <a.div style={styles} className="rounded-lg ring-2 ring-gray-300 bg-purple-400 scale-50">
             <a.div className="p-4 w-32 h-32 grid grid-cols-3 grid-rows-3 gap-2">
                 {items.map((on: number, i: number) => (
                     <div className={`w-full h-full rounded-full ${on ? 'bg-purple-900' : 'bg-transparent'}`} key={i} />
@@ -51,25 +49,6 @@ function TestCubeAnimation() {
     const ANGLES = [180, -90, 90, -90, 90, 0]; // k:back l:left t:top b:bottom r:right f:front
     const faceStyle = (idx: number, move: number): string => `rotate${ANGLES_AXIS[idx]}(${ANGLES[idx]}deg) translateZ(${move}px)`;
 
-    let f1k = {
-        transform: `rotateX(180deg) translateZ(${dieSize / 2}px)`,
-    };
-    let f2l = {
-        transform: `rotateY(-90deg) translateZ(${dieSize / 2}px)`,
-    };
-    let f3t = {
-        transform: `rotateX(90deg) translateZ(${dieSize / 2}px)`,
-    };
-    let f4b = {
-        transform: `rotateX(-90deg) translateZ(${dieSize / 2}px)`,
-    };
-    let f5r = {
-        transform: `rotateY(90deg) translateZ(${dieSize / 2}px)`,
-    };
-    let f6f = {
-        transform: `rotateY(0deg) translateZ(${dieSize / 2}px)`,
-    };
-
     const [styles, api] = useSpring(() => ({
         num: 0,
         transform: 'rotateX(75deg) rotateY(0deg) rotateZ(45deg)',
@@ -86,7 +65,7 @@ function TestCubeAnimation() {
                 //await next({ backgroundColor: 'rgb(76, 29, 149)' });
                 await next({
                     transform: `rotateX(${angle}deg) rotateY(${angle}deg) rotateZ(${angle}deg)`, config: { duration: 1000 }
-                    // transform: `rotateX(720deg) rotateY(720deg) rotateZ(720deg)`
+                    // transform: `rotateX(720deg) rotateY(720deg) rotateZ(720deg)`, config: { duration: 1000 }
                 });
                 setDigit(randomIntInclusive(1, 6));
                 await next({
@@ -94,45 +73,27 @@ function TestCubeAnimation() {
                     transform: 'rotateX(0deg) rotateY(0deg) rotateZ(0deg)'
                 });
                 //await next({ backgroundColor: 'rgb(167, 139, 250)', config: { duration: 100 } });
-
-
-            },
-            onRest: () => {
-                //setDigit(randomIntInclusive(1, 6));
             }
         });
     }
 
     return (
-        <div
-            className="scale-50"
-            onClick={spin}
-        >
+        <div className="scale-50" onClick={spin}>
             <a.div
                 className="mb-4 w-32 h-32 relative"
                 style={{
                     transformStyle: 'preserve-3d',
-                    //transform: 'rotateX(75deg) rotateY(0deg) rotateZ(45deg)',
                     transformOrigin: 'center center',
                     perspectiveOrigin: '50% 50%',
                     //perspective: '300px',
                     ...styles
                 }}
             >
-                <div style={{ ...f1k }} className="w-32 h-32 absolute"> <Face digit={(digit + 0) % 6 + 1} /> </div>
-                <div style={{ ...f2l }} className="w-32 h-32 absolute"> <Face digit={(digit + 1) % 6 + 1} /> </div>
-                <div style={{ ...f3t }} className="w-32 h-32 absolute"> <Face digit={(digit + 2) % 6 + 1} /> </div>
-                <div style={{ ...f4b }} className="w-32 h-32 absolute"> <Face digit={(digit + 3) % 6 + 1} /> </div>
-                <div style={{ ...f5r }} className="w-32 h-32 absolute"> <Face digit={(digit + 4) % 6 + 1} /> </div>
-                <div style={{ ...f6f }} className="w-32 h-32 absolute"> <Face digit={(digit + 5) % 6 + 1} /> </div>
-
-                {/* <div style={{ ...f1k }} className="w-32 h-32 absolute"> <Face digit={(digit + 0) % 6 + 1} /> </div>
-                <div style={{ ...f2l }} className="w-32 h-32 absolute"> <Face digit={(digit + 1) % 6 + 1} /> </div>
-                <div style={{ ...f3t }} className="w-32 h-32 absolute"> <Face digit={(digit + 2) % 6 + 1} /> </div>
-                <div style={{ ...f4b }} className="w-32 h-32 absolute"> <Face digit={(digit + 3) % 6 + 1} /> </div>
-                <div style={{ ...f5r }} className="w-32 h-32 absolute"> <Face digit={(digit + 4) % 6 + 1} /> </div>
-                <div style={{ ...f6f }} className="w-32 h-32 absolute"> <Face digit={(digit + 5) % 6 + 1} /> </div> */}
-
+                {ANGLES.map((_angle: number, idx: number) => (
+                    <div style={{ transform: faceStyle(idx, dieSize / 2) }} className="w-32 h-32 absolute" key={idx}>
+                        <Face digit={(digit + idx) % 6 + 1} />
+                    </div>
+                ))}
             </a.div>
         </div>
     );
