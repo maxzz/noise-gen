6d0a0fd64402a5fbbce0d5a635c81dad7f032118
Max Zakharzhevskiy <maxzz@msn.com>
2021-07-05 19:15:20 -0700

    WIP Cube animation. back to the fixed dots size

--- src/components/TestCubeAnimation.tsx
+++ src/components/TestCubeAnimation.tsx
@@ -23,13 +23,11 @@ function Face({ digit, style = {} }: { digit: number; style: React.CSSProperties
         return items;
     }, [digit]);
     return (
-        <div className={
-            `absolute
+        <div className="absolute
             p-4
             w-full h-full
             rounded-lg ring-2 ring-gray-300 bg-purple-400
-            grid grid-cols-3 grid-rows-3 ${digit === 6 ? 'gap-2' : ''}`
-        }
+            grid grid-cols-3 grid-rows-3 gap-2"
             style={style}
         >
             {items.map((on: number, i: number) => (
