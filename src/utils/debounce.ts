7b7511faf1210ca42e401bb0fb0f491e603c2758
Max Zakharzhevskiy <maxzz@msn.com>
2021-08-12 00:58:26 -0700

    Fix. add strongly typed debounce

--- src/utils/debounce.ts
+++ src/utils/debounce.ts
@@ -1,10 +1,9 @@
-export default function debounce(f: Function, timeout = 100) {
+export default function debounce<T extends (...args: any[]) => any>(f: T, timeout = 100) {
     let timer: any;
     let lastArgs: any;
     let lastThis: any;
 
-    return function (...args: any[]) {
-        //@ts-ignore
+    return function (this: any, ...args: Parameters<T>) {
         lastThis = this;
         lastArgs = args;
         if (timer) {
