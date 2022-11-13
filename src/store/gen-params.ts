f81d32bf6001cef53c32ad5fa2dc6fc60cbacdde
Max Zakharzhevskiy <maxzz@msn.com>
2022-11-12 18:12:44 -0800

    WIP. store. add gen-params.ts

---
+++ src/store/gen-params.ts
@@ -0,0 +1,28 @@
+import { atom } from "jotai";
+import { atomWithCallback } from "../hooks/atomsX";
+import { defAppSettings, storeAppParams } from "../utils/storageAppConfig";
+import { GenParams } from "../utils/types";
+
+// GenParams
+
+export const GenParamsAtom = atomWithCallback<GenParams>(defAppSettings.renderParams.genParams, (get, _) => storeAppParams(get));
+
+export const N1Atom = atom(
+    (get) => get(GenParamsAtom).n1,
+    (get, set, update: number) => set(GenParamsAtom, { ...get(GenParamsAtom), n1: update })
+);
+
+export const N2Atom = atom(
+    (get) => get(GenParamsAtom).n2,
+    (get, set, update: number) => set(GenParamsAtom, { ...get(GenParamsAtom), n2: update })
+);
+
+export const DistortionAtom = atom(
+    (get) => get(GenParamsAtom).distortion,
+    (get, set, update: number) => set(GenParamsAtom, { ...get(GenParamsAtom), distortion: update })
+);
+
+export const DotDiameterAtom = atom(
+    (get) => get(GenParamsAtom).dotDiameter,
+    (get, set, update: number) => set(GenParamsAtom, { ...get(GenParamsAtom), dotDiameter: update })
+);
