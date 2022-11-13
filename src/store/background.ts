56fdf6b0c61a2faccf0daef824289d04d299d49e
Max Zakharzhevskiy <maxzz@msn.com>
2022-11-12 18:26:57 -0800

    WIP. store. background.ts

---
+++ src/store/background.ts
@@ -0,0 +1,24 @@
+import { atom } from "jotai";
+
+//#region Application background
+
+// Set Application background
+
+const AppBackgroundUrlRawAtom = atom('');
+
+export const AppBackgroundUrlAtom = atom(
+    (get) => get(AppBackgroundUrlRawAtom),
+    (get, set, blob: Blob | null) => {
+        let current = get(AppBackgroundUrlRawAtom);
+        if (current) {
+            window.URL.revokeObjectURL(current);
+        }
+        set(AppBackgroundUrlRawAtom, blob ? window.URL.createObjectURL(blob) : '');
+    }
+);
+
+export const AppBackgroundActiveAtom = atom(
+    (get) => !!get(AppBackgroundUrlRawAtom)
+);
+
+//#endregion Application background
