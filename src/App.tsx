7c051759a090d60e845e492b85a51da48e3b669f
Max Zakharzhevskiy <maxzz@msn.com>
2022-11-13 14:05:21 -0800

    WIP. app. move AppBackground to own component file

--- src/App.tsx
+++ src/App.tsx
@@ -1,35 +1,11 @@
-import React, { HTMLAttributes } from 'react';
-import { useAtom } from 'jotai';
-import { AppBackgroundActiveAtom, AppBackgroundUrlAtom } from './store';
+import React from 'react';
 import Canvas from './components/RenderCanvas';
 import PreciseControls from './components/PreciseControls';
 import MainControls from './components/MainControls';
 //import SortedList from './components/UI/DraggableList/TestSortedList';
-import './App.scss';
+import { AppBackground } from './components/UI/AppBackground';
 import { Section1_Header } from './components/Section1_Header';
-
-function AppBackground({ children }: HTMLAttributes<HTMLDivElement>) {
-    const [appBackgroundUrl, setAppBackgroundUrl] = useAtom(AppBackgroundUrlAtom);
-    const [appBackgroundActive] = useAtom(AppBackgroundActiveAtom);
-    return (
-        <div
-            className="App h-screen flex flex-col items-center space-y-4 bg-purple-200 color-vars"
-            // style={{background: `radial-gradient(circle, #d5ccf7 0%, #ab9dde 100%)`}}
-            // style={{backgroundImage: `url(${bkgImage}), radial-gradient(circle, #d5ccf7 0%, #ab9dde 100%)`, backgroundBlendMode: 'multiply, screen, color-dodge'}}
-            style={
-                appBackgroundActive
-                    ? {
-                        backgroundImage: `url(${appBackgroundUrl}), radial-gradient(circle, #d5ccf7 0%, #ab9dde 100%)`,
-                        backgroundBlendMode: 'multiply, screen, color-dodge',
-                    } : {
-                        backgroundImage: `radial-gradient(circle, #d5ccf7 0%, #ab9dde 100%)`,
-                    }
-            }
-        >
-            {children}
-        </div>
-    );
-}
+import './App.scss';
 
 export function App() {
     return (
---
+++ src/components/UI/AppBackground.tsx
@@ -0,0 +1,26 @@
+import React, { HTMLAttributes } from "react";
+import { useAtom } from "jotai";
+import { AppBackgroundActiveAtom, AppBackgroundUrlAtom } from "../../store";
+
+export function AppBackground({ children }: HTMLAttributes<HTMLDivElement>) {
+    const [appBackgroundUrl, setAppBackgroundUrl] = useAtom(AppBackgroundUrlAtom);
+    const [appBackgroundActive] = useAtom(AppBackgroundActiveAtom);
+    return (
+        <div
+            className="App h-screen flex flex-col items-center space-y-4 bg-purple-200 color-vars"
+            // style={{background: `radial-gradient(circle, #d5ccf7 0%, #ab9dde 100%)`}}
+            // style={{backgroundImage: `url(${bkgImage}), radial-gradient(circle, #d5ccf7 0%, #ab9dde 100%)`, backgroundBlendMode: 'multiply, screen, color-dodge'}}
+            style={
+                appBackgroundActive
+                    ? {
+                        backgroundImage: `url(${appBackgroundUrl}), radial-gradient(circle, #d5ccf7 0%, #ab9dde 100%)`,
+                        backgroundBlendMode: 'multiply, screen, color-dodge',
+                    } : {
+                        backgroundImage: `radial-gradient(circle, #d5ccf7 0%, #ab9dde 100%)`,
+                    }
+            }
+        >
+            {children}
+        </div>
+    );
+}
