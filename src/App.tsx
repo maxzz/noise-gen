c2ab2e6e612ac308bc55204cf9e2bc6c58d28c64
Max Zakharzhevskiy <maxzz@msn.com>
2022-11-13 14:21:38 -0800

    WIP. app. move Section2_Main to own component file

--- src/App.tsx
+++ src/App.tsx
@@ -1,34 +1,14 @@
 import React from 'react';
 import { AppBackground } from './components/UI/AppBackground';
 import { Section1_Header } from './components/Section1_Header';
-import { MainControls } from './components/MainControls';
-import { PreciseControls } from './components/PreciseControls';
-import { Canvas } from './components/RenderCanvas';
-//import SortedList from './components/UI/DraggableList/TestSortedList';
+import { Section2_Main } from './components/Section2_Main';
 import './App.scss';
 
 export function App() {
     return (
         <AppBackground>
             <Section1_Header />
-
-            <div className="relative max-w-md w-full flex-1 flex flex-col items-center">
-                {/* <div className="">
-                    <SortedList />
-                </div> */}
-
-                {/* Controls */}
-                <div className="w-full flex flex-col space-y-1">
-                    <MainControls />
-                    <PreciseControls />
-                </div>
-
-                {/* Canvas */}
-                <div className="flex-1 mt-1">
-                    <Canvas />
-                </div>
-            </div>
-
+            <Section2_Main />
         </AppBackground>
     );
 }
---
+++ src/components/Section2_Main/index.tsx
@@ -0,0 +1,26 @@
+import React from 'react';
+import { MainControls } from '../MainControls';
+import { PreciseControls } from '../PreciseControls';
+import { Canvas } from '../RenderCanvas';
+//import SortedList from '../UI/DraggableList/TestSortedList';
+
+export function Section2_Main() {
+    return (
+        <div className="relative max-w-md w-full flex-1 flex flex-col items-center">
+            {/* <div className="">
+                <SortedList />
+            </div> */}
+
+            {/* Controls */}
+            <div className="w-full flex flex-col space-y-1">
+                <MainControls />
+                <PreciseControls />
+            </div>
+
+            {/* Canvas */}
+            <div className="flex-1 mt-1">
+                <Canvas />
+            </div>
+        </div>
+    );
+}
