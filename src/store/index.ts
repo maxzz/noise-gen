e487a30d510337922a2340ee53d87ae3dbaf7ff7
Max Zakharzhevskiy <maxzz@msn.com>
2022-11-12 18:29:24 -0800

    WIP. store. index.ts clean up

--- src/store/index.ts
+++ src/store/index.ts
@@ -1,23 +1,8 @@
-import { atom } from 'jotai';
-import { atomWithCallback } from '../hooks/atomsX';
-import { WorkerEx } from '../hooks/useCanvasWorker';
-import { GENPARAMS, GenParams, I2W, I4W, NOISEPARAMS, PresetData, PRESET_H, PRESET_W, RenderParams } from '../utils/types';
-import { defAppSettings, storeAppParams } from '../utils/storageAppConfig';
-import { defPresets, storePresets } from '../utils/storagePresets';
-import uuid from '../utils/uuid';
-import { random, withDigits } from '../utils/numbers';
-
-//#region Offscreen canvas and Worker
-
 export {
     OffscreenCanvasAtom,
     RenderWorkerAtom,
     ManualSizeAtom,
 } from './canvas';
-
-//#endregion Offscreen canvas and Worker
-
-//#region Generator current params
 
 // GenParams
 
@@ -45,7 +30,6 @@ export {
     GeneratePresetAtom,
 } from './editor-params';
 
-
 // Noise Editor
 
 export {
@@ -55,9 +39,7 @@ export {
     ShowNoiseEditorAtom,
 } from './noise-editor';
 
-//#endregion Generator current params
-
-//#region Presets
+// Presets
 
 export {
     PresetsAtom,
@@ -67,15 +49,9 @@ export {
     CreateAppendPresetAtom,
 } from './presets';
 
-//#endregion Presets
-
-//#region Application background
-
-// Set Application background
+// Application background
 
 export {
     AppBackgroundUrlAtom,
     AppBackgroundActiveAtom,
 } from './background';
-
-//#endregion Application background
