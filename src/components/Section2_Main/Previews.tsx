2583dfcb547440051a2446de89b90cb1c78e03cf
Max Zakharzhevskiy <maxzz@msn.com>
2022-11-15 16:32:00 -0800

    WIP. main. update PreviewBox

--- src/components/Section2_Main/Previews.tsx
+++ src/components/Section2_Main/Previews.tsx
@@ -1,9 +1,10 @@
-import React, { SVGAttributes } from 'react';
+import React from 'react';
 import { useAtomValue, useSetAtom } from 'jotai';
 import { PresetData, PRESET_H, PRESET_W } from '@/store/types/app-types';
 import { PresetsAtom, RemovePresetAtom, SetRenderParamsAtom } from '@/store';
-import styles from './Previews.module.scss';
 import { classNames } from '@/utils';
+import styles from './Previews.module.scss';
+import { IconCross, IconTrash } from '@/components/UI/Icons';
 
 interface PreviewBoxProps {
     item: PresetData;
@@ -11,42 +12,25 @@ interface PreviewBoxProps {
     selectItem: (item: PresetData) => void;
 }
 
-function IconTrash({className, ...rest}: SVGAttributes<SVGSVGElement>) {
-    return (
-        <svg className={classNames("stroke-current fill-transparent", className)} strokeLinecap="round" strokeLinejoin="round" strokeWidth={2} viewBox="0 0 24 24" {...rest}>
-            <path d="M19 7l-.867 12.142A2 2 0 0116.138 21H7.862a2 2 0 01-1.995-1.858L5 7m5 4v6m4-6v6m1-10V4a1 1 0 00-1-1h-4a1 1 0 00-1 1v3M4 7h16" />
-        </svg>
-    );
-}
-
-function IconCross({className, ...rest}: SVGAttributes<SVGSVGElement>) {
-    return (
-        <svg className={classNames("stroke-current fill-transparent", className)} viewBox="0 0 24 24" {...rest}>
-        <path strokeLinecap="round" strokeWidth={4} d="M6 18L18 6M6 6l12 12" />
-        </svg>
-    );
-}
-
 function PreviewBox({ item, deleteItem, selectItem }: PreviewBoxProps) {
     return (
-        <div className={`${styles.preset} px-1 py-2 cursor-pointer select-none transform active-scale`} onClick={() => selectItem(item)}>
-            <div className="relative border-4 border-gray-50" style={{ width: `${PRESET_W + 8}px`, height: `${PRESET_H + 8}px` }}>
-                {/* +8 for double border size */}
-                {item.preview && <img
-                    className={`${styles.maybeBroken} w-full h-full object-cover`}
-                    src={item.preview} alt="preset"
-                />}
-
+        <div className={`px-1 py-2 cursor-pointer select-none transform active-scale ${styles.preset}`} onClick={() => selectItem(item)}>
+            <div className="relative border-4 border-gray-50" style={{ width: `${PRESET_W + 8}px`, height: `${PRESET_H + 8}px` }}> {/* +8 for double border size */}
                 {item.preview &&
-                    <div
-                        className={`${styles.removePreset} absolute p-1 -top-3 -right-3 
-                        border rounded-full text-gray-500 border-gray-500 bg-gray-50`}
-                        title="Remove preset"
-                        onClick={(event) => { event.stopPropagation(); deleteItem(item.id); }}
-                    >
-                        <IconCross className="h-3 w-3" />
-                        <IconTrash className="h-3 w-3" />
-                    </div>
+                    <>
+                        <img
+                            className={`w-full h-full object-cover ${styles.maybeBroken}`}
+                            src={item.preview} alt="preset"
+                        />
+                        <div
+                            className={`absolute p-1 -top-3 -right-3 border rounded-full text-gray-500 border-gray-500 bg-gray-50 ${styles.removePreset}`}
+                            title="Remove preset"
+                            onClick={(event) => { event.stopPropagation(); deleteItem(item.id); }}
+                        >
+                            {/* <IconCross className="h-3 w-3" /> */}
+                            <IconTrash className="h-3 w-3" />
+                        </div>
+                    </>
                 }
             </div>
         </div>
---
+++ src/components/UI/Icons/index.tsx
@@ -0,0 +1,18 @@
+import React, { SVGAttributes } from "react";
+import { classNames } from "@/utils";
+
+export function IconTrash({className, ...rest}: SVGAttributes<SVGSVGElement>) {
+    return (
+        <svg className={classNames("stroke-current fill-transparent", className)} strokeLinecap="round" strokeLinejoin="round" strokeWidth={2} viewBox="0 0 24 24" {...rest}>
+            <path d="M19 7l-.867 12.142A2 2 0 0116.138 21H7.862a2 2 0 01-1.995-1.858L5 7m5 4v6m4-6v6m1-10V4a1 1 0 00-1-1h-4a1 1 0 00-1 1v3M4 7h16" />
+        </svg>
+    );
+}
+
+export function IconCross({className, ...rest}: SVGAttributes<SVGSVGElement>) {
+    return (
+        <svg className={classNames("stroke-current fill-transparent", className)} viewBox="0 0 24 24" {...rest}>
+        <path strokeLinecap="round" strokeWidth={4} d="M6 18L18 6M6 6l12 12" />
+        </svg>
+    );
+}
