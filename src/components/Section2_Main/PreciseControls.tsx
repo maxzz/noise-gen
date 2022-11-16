9495a19087d7a79ce6f8d86dc9f1f6daff442ee9
Max Zakharzhevskiy <maxzz@msn.com>
2022-11-15 16:13:31 -0800

    WIP. main. update Previews component

--- src/components/Section2_Main/PreciseControls.tsx
+++ src/components/Section2_Main/PreciseControls.tsx
@@ -1,14 +1,14 @@
 import React from 'react';
 import { PreciseControlsSliders } from './PreciseControlsSliders';
 import { PreciseControlsActions } from './PreciseControlsActions';
-import { PreciseControlsPreviews } from './PreciseControlsPreviews';
+import { Previews } from './Previews';
 
 export function PreciseControls() {
     return (
         <div className="py-2 bg-purple-100 border rounded border-gray-400 select-none shadow">
             <PreciseControlsSliders />
             <PreciseControlsActions />
-            <PreciseControlsPreviews />
+            <Previews />
         </div>
     );
 }
