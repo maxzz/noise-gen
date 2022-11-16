d1968eaa70cc838a6555470a711af7bf1e9d36ec
Max Zakharzhevskiy <maxzz@msn.com>
2022-11-15 16:37:15 -0800

    WIP. main. update PreviewBox. 2

--- src/components/Section2_Main/Previews.tsx
+++ src/components/Section2_Main/Previews.tsx
@@ -2,33 +2,24 @@ import React from 'react';
 import { useAtomValue, useSetAtom } from 'jotai';
 import { PresetData, PRESET_H, PRESET_W } from '@/store/types/app-types';
 import { PresetsAtom, RemovePresetAtom, SetRenderParamsAtom } from '@/store';
-import { classNames } from '@/utils';
-import styles from './Previews.module.scss';
 import { IconCross, IconTrash } from '@/components/UI/Icons';
+import styles from './Previews.module.scss';
 
-interface PreviewBoxProps {
-    item: PresetData;
-    deleteItem: (id: string) => void;
-    selectItem: (item: PresetData) => void;
-}
-
-function PreviewBox({ item, deleteItem, selectItem }: PreviewBoxProps) {
+function PreviewBox({ item }: {item: PresetData}) {
+    const setRenderParams = useSetAtom(SetRenderParamsAtom);
+    const removePreset = useSetAtom(RemovePresetAtom);
     return (
-        <div className={`px-1 py-2 cursor-pointer select-none transform active-scale ${styles.preset}`} onClick={() => selectItem(item)}>
+        <div className={`px-1 py-2 cursor-pointer select-none transform active-scale ${styles.preset}`} onClick={() => setRenderParams(item.renderParams)}>
             <div className="relative border-4 border-gray-50" style={{ width: `${PRESET_W + 8}px`, height: `${PRESET_H + 8}px` }}> {/* +8 for double border size */}
                 {item.preview &&
                     <>
-                        <img
-                            className={`w-full h-full object-cover ${styles.maybeBroken}`}
-                            src={item.preview} alt="preset"
-                        />
+                        <img className={`w-full h-full object-cover ${styles.maybeBroken}`} src={item.preview} alt="preset"/>
                         <div
                             className={`absolute p-1 -top-3 -right-3 border rounded-full text-gray-500 border-gray-500 bg-gray-50 ${styles.removePreset}`}
                             title="Remove preset"
-                            onClick={(event) => { event.stopPropagation(); deleteItem(item.id); }}
+                            onClick={(event) => { event.stopPropagation(); removePreset(item.id); }}
                         >
-                            {/* <IconCross className="h-3 w-3" /> */}
-                            <IconTrash className="h-3 w-3" />
+                            <IconTrash className="h-3 w-3" /> {/* <IconCross className="h-3 w-3" /> */}
                         </div>
                     </>
                 }
@@ -39,26 +30,11 @@ function PreviewBox({ item, deleteItem, selectItem }: PreviewBoxProps) {
 
 export function Previews() {
     const presets = useAtomValue(PresetsAtom);
-    const setRenderParams = useSetAtom(SetRenderParamsAtom);
-    const removePreset = useSetAtom(RemovePresetAtom);
-
-    function deleteItem(id: string) {
-        removePreset(id);
-    }
-
-    function selectItem(item: PresetData) {
-        setRenderParams(item.renderParams);
-    }
 
     return (
         <div className="px-1 flex flex-wrap">
             {presets.map((preset) => (
-                <PreviewBox
-                    item={preset}
-                    deleteItem={deleteItem}
-                    selectItem={selectItem}
-                    key={preset.id}
-                />
+                <PreviewBox item={preset} key={preset.id} />
             ))}
         </div>
     );
