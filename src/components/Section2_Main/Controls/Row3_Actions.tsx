baff5c9650032b53530f2919ca429feff9726269
Max Zakharzhevskiy <maxzz@msn.com>
2022-11-17 14:40:52 -0800

    WIP. Row3_Actions. extract built in icons

--- src/components/Section2_Main/Controls/Row3_Actions.tsx
+++ src/components/Section2_Main/Controls/Row3_Actions.tsx
@@ -54,9 +54,8 @@ export function Row3_Actions() {
         <div className="p-2 flex space-x-1">
 
             {/* Canvas size buttons */}
-            <div className="px-1 flex-centered space-x-1 
-                border rounded border-gray-400 bg-gray-50 hover:bg-white
-                "
+            <div
+                className="px-1 flex-centered space-x-1 border rounded border-gray-400 bg-gray-50 hover:bg-white"
                 style={{ boxShadow: '#00000014 1px 1px 0px 0px' }}
             >
                 <Row3_SizeBoxes />
@@ -72,9 +71,7 @@ export function Row3_Actions() {
                 title="Set canvas image as application background (F2)"
                 onClick={setAsBackground}
             >
-                <svg className="h-6 w-8" fill="none" viewBox="0 0 24 24" stroke="currentColor">
-                    <path strokeLinecap="round" strokeLinejoin="round" strokeWidth={.8} d="M4 16l4.586-4.586a2 2 0 012.828 0L16 16m-2-2l1.586-1.586a2 2 0 012.828 0L20 14m-6-6h.01M6 20h12a2 2 0 002-2V6a2 2 0 00-2-2H6a2 2 0 00-2 2v12a2 2 0 002 2z" />
-                </svg>
+                <IconMountains />
             </div>
 
             {/* Preset add */}
@@ -87,9 +84,7 @@ export function Row3_Actions() {
                 title="Save preset"
                 onClick={appendNew}
             >
-                <svg className="h-6 w-6" fill="none" viewBox="0 0 24 24" stroke="currentColor">
-                    <path strokeLinecap="round" strokeLinejoin="round" strokeWidth={.8} d="M17 14v6m-3-3h6M6 10h2a2 2 0 002-2V6a2 2 0 00-2-2H6a2 2 0 00-2 2v2a2 2 0 002 2zm10 0h2a2 2 0 002-2V6a2 2 0 00-2-2h-2a2 2 0 00-2 2v2a2 2 0 002 2zM6 20h2a2 2 0 002-2v-2a2 2 0 00-2-2H6a2 2 0 00-2 2v2a2 2 0 002 2z" />
-                </svg>
+                <IconAdd />
             </div>
 
             {/* Image save */}
@@ -104,9 +99,7 @@ export function Row3_Actions() {
                     title="Save image"
                     onClick={() => setShowSelectFileSize((prev) => !prev)}
                 >
-                    <svg className="h-6 w-6" fill="none" viewBox="0 0 24 24" stroke="currentColor">
-                        <path strokeLinecap="round" strokeLinejoin="round" strokeWidth={.8} d="M8 7H5a2 2 0 00-2 2v9a2 2 0 002 2h14a2 2 0 002-2V9a2 2 0 00-2-2h-3m-1 4l-3 3m0 0l-3-3m3 3V4" />
-                    </svg>
+                    <IconSave />
                 </div>
                 {/* Popup */}
                 {showSelectFileSize &&
@@ -116,5 +109,29 @@ export function Row3_Actions() {
                 }
             </div>
         </div>
+    );
+}
+
+function IconMountains() {
+    return (
+        <svg className="h-6 w-8" fill="none" viewBox="0 0 24 24" stroke="currentColor">
+            <path strokeLinecap="round" strokeLinejoin="round" strokeWidth={.8} d="M4 16l4.586-4.586a2 2 0 012.828 0L16 16m-2-2l1.586-1.586a2 2 0 012.828 0L20 14m-6-6h.01M6 20h12a2 2 0 002-2V6a2 2 0 00-2-2H6a2 2 0 00-2 2v12a2 2 0 002 2z" />
+        </svg>
+    );
+}
+
+function IconAdd() {
+    return (
+        <svg className="h-6 w-6" fill="none" viewBox="0 0 24 24" stroke="currentColor">
+            <path strokeLinecap="round" strokeLinejoin="round" strokeWidth={.8} d="M17 14v6m-3-3h6M6 10h2a2 2 0 002-2V6a2 2 0 00-2-2H6a2 2 0 00-2 2v2a2 2 0 002 2zm10 0h2a2 2 0 002-2V6a2 2 0 00-2-2h-2a2 2 0 00-2 2v2a2 2 0 002 2zM6 20h2a2 2 0 002-2v-2a2 2 0 00-2-2H6a2 2 0 00-2 2v2a2 2 0 002 2z" />
+        </svg>
+    );
+}
+
+function IconSave() {
+    return (
+        <svg className="h-6 w-6" fill="none" viewBox="0 0 24 24" stroke="currentColor">
+            <path strokeLinecap="round" strokeLinejoin="round" strokeWidth={.8} d="M8 7H5a2 2 0 00-2 2v9a2 2 0 002 2h14a2 2 0 002-2V9a2 2 0 00-2-2h-3m-1 4l-3 3m0 0l-3-3m3 3V4" />
+        </svg>
     );
 }
