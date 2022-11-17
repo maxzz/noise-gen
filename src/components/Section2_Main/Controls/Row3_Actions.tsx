ea60a7a673a2847fbcaafaf22ae3034a8c0642a4
Max Zakharzhevskiy <maxzz@msn.com>
2022-11-17 14:48:15 -0800

    WIP. Row3_Actions. move icons size definition outside

--- src/components/Section2_Main/Controls/Row3_Actions.tsx
+++ src/components/Section2_Main/Controls/Row3_Actions.tsx
@@ -1,4 +1,4 @@
-import React from 'react';
+import React, { SVGAttributes } from 'react';
 import { useAtomValue, useSetAtom } from 'jotai';
 import { useSpring, a } from '@react-spring/web';
 import { useKey } from 'react-use';
@@ -7,6 +7,7 @@ import { I2W, PRESET_H, PRESET_W, WH } from '@/store/types';
 import { Row3_SizeBoxes } from './Row3_SizeBoxes';
 import { Row3_PopupImageSize } from './Row3_PopupImageSize';
 import { saveBlobData } from '@/utils/saveImage';
+import { classNames } from '@/utils';
 
 export function Row3_Actions() {
     const worker = useAtomValue(RenderWorkerAtom);
@@ -71,7 +72,7 @@ export function Row3_Actions() {
                 title="Set canvas image as application background (F2)"
                 onClick={setAsBackground}
             >
-                <IconMountains />
+                <IconMountains className="w-8 h-6" />
             </div>
 
             {/* Preset add */}
@@ -84,7 +85,7 @@ export function Row3_Actions() {
                 title="Save preset"
                 onClick={appendNew}
             >
-                <IconAdd />
+                <IconAdd className="w-6 h-6" />
             </div>
 
             {/* Image save */}
@@ -99,7 +100,7 @@ export function Row3_Actions() {
                     title="Save image"
                     onClick={() => setShowSelectFileSize((prev) => !prev)}
                 >
-                    <IconSave />
+                    <IconSave className="w-6 h-6" />
                 </div>
                 {/* Popup */}
                 {showSelectFileSize &&
@@ -112,26 +113,26 @@ export function Row3_Actions() {
     );
 }
 
-function IconMountains() {
+function IconMountains({className, ...rest}: SVGAttributes<SVGSVGElement>) {
     return (
-        <svg className="h-6 w-8" fill="none" viewBox="0 0 24 24" stroke="currentColor">
-            <path strokeLinecap="round" strokeLinejoin="round" strokeWidth={.8} d="M4 16l4.586-4.586a2 2 0 012.828 0L16 16m-2-2l1.586-1.586a2 2 0 012.828 0L20 14m-6-6h.01M6 20h12a2 2 0 002-2V6a2 2 0 00-2-2H6a2 2 0 00-2 2v12a2 2 0 002 2z" />
+        <svg className={classNames("fill-transparent stroke-current", className)} strokeLinecap="round" strokeLinejoin="round" strokeWidth={.8} viewBox="0 0 24 24" {...rest}>
+            <path d="M4 16l4.586-4.586a2 2 0 012.828 0L16 16m-2-2l1.586-1.586a2 2 0 012.828 0L20 14m-6-6h.01M6 20h12a2 2 0 002-2V6a2 2 0 00-2-2H6a2 2 0 00-2 2v12a2 2 0 002 2z" />
         </svg>
     );
 }
 
-function IconAdd() {
+function IconAdd({className, ...rest}: SVGAttributes<SVGSVGElement>) {
     return (
-        <svg className="h-6 w-6" fill="none" viewBox="0 0 24 24" stroke="currentColor">
-            <path strokeLinecap="round" strokeLinejoin="round" strokeWidth={.8} d="M17 14v6m-3-3h6M6 10h2a2 2 0 002-2V6a2 2 0 00-2-2H6a2 2 0 00-2 2v2a2 2 0 002 2zm10 0h2a2 2 0 002-2V6a2 2 0 00-2-2h-2a2 2 0 00-2 2v2a2 2 0 002 2zM6 20h2a2 2 0 002-2v-2a2 2 0 00-2-2H6a2 2 0 00-2 2v2a2 2 0 002 2z" />
+        <svg className={classNames("fill-transparent stroke-current", className)} strokeLinecap="round" strokeLinejoin="round" strokeWidth={.8} viewBox="0 0 24 24" {...rest}>
+            <path d="M17 14v6m-3-3h6M6 10h2a2 2 0 002-2V6a2 2 0 00-2-2H6a2 2 0 00-2 2v2a2 2 0 002 2zm10 0h2a2 2 0 002-2V6a2 2 0 00-2-2h-2a2 2 0 00-2 2v2a2 2 0 002 2zM6 20h2a2 2 0 002-2v-2a2 2 0 00-2-2H6a2 2 0 00-2 2v2a2 2 0 002 2z" />
         </svg>
     );
 }
 
-function IconSave() {
+function IconSave({className, ...rest}: SVGAttributes<SVGSVGElement>) {
     return (
-        <svg className="h-6 w-6" fill="none" viewBox="0 0 24 24" stroke="currentColor">
-            <path strokeLinecap="round" strokeLinejoin="round" strokeWidth={.8} d="M8 7H5a2 2 0 00-2 2v9a2 2 0 002 2h14a2 2 0 002-2V9a2 2 0 00-2-2h-3m-1 4l-3 3m0 0l-3-3m3 3V4" />
+        <svg className={classNames("fill-transparent stroke-current", className)} strokeLinecap="round" strokeLinejoin="round" strokeWidth={.8} viewBox="0 0 24 24" {...rest}>
+            <path d="M8 7H5a2 2 0 00-2 2v9a2 2 0 002 2h14a2 2 0 002-2V9a2 2 0 00-2-2h-3m-1 4l-3 3m0 0l-3-3m3 3V4" />
         </svg>
     );
 }
