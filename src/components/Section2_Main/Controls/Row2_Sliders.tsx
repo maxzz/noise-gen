ad097f1109ddd57e0a47509793581ee8fc93f9b7
Max Zakharzhevskiy <maxzz@msn.com>
2022-11-20 15:56:54 -0800

    WIP. Row2_Sliders.tsx. code clean up

--- src/components/Section2_Main/Controls/Row2_Sliders.tsx
+++ src/components/Section2_Main/Controls/Row2_Sliders.tsx
@@ -8,65 +8,42 @@ import { Slider } from '@/components/UI/Slider';
 function Sliders1() {
     const [n1, setN1] = useAtom(N1Atom);
     return (
-        <>
-            <Slider min={GENPARAMS.min.n1} max={GENPARAMS.max.n1} value={n1} onChange={setN1} label="Noise 1" />
-        </>
+        <Slider min={GENPARAMS.min.n1} max={GENPARAMS.max.n1} value={n1} onChange={setN1} label="Noise 1" />
     );
 }
 
 function Sliders2() {
     const [n2, setN2] = useAtom(N2Atom);
     return (
-        <>
-            <Slider min={GENPARAMS.min.n2} max={GENPARAMS.max.n2} value={n2} onChange={setN2} label="Noise 2" />
-        </>
+        <Slider min={GENPARAMS.min.n2} max={GENPARAMS.max.n2} value={n2} onChange={setN2} label="Noise 2" />
     );
 }
 
 function Sliders3() {
     const [distortion, setDistortion] = useAtom(DistortionAtom);
     return (
-        <>
-            <Slider min={GENPARAMS.min.distortion} max={GENPARAMS.max.distortion} value={distortion} onChange={setDistortion} label="Distortion" />
-        </>
+        <Slider min={GENPARAMS.min.distortion} max={GENPARAMS.max.distortion} value={distortion} onChange={setDistortion} label="Distortion" />
     );
 }
 
 function Sliders4() {
     const [dotDiameter, setDotDiameter] = useAtom(DotDiameterAtom);
     return (
-        <>
-            <Slider min={GENPARAMS.min.dotDiameter} max={GENPARAMS.max.dotDiameter} value={dotDiameter} onChange={setDotDiameter} label="Dot diameter" />
-        </>
+        <Slider min={GENPARAMS.min.dotDiameter} max={GENPARAMS.max.dotDiameter} value={dotDiameter} onChange={setDotDiameter} label="Dot diameter" />
     );
 }
-
-// function Sliders() {
-//     const [n1, setN1] = useAtom(N1Atom);
-//     const [n2, setN2] = useAtom(N2Atom);
-//     const [distortion, setDistortion] = useAtom(DistortionAtom);
-//     const [dotDiameter, setDotDiameter] = useAtom(DotDiameterAtom);
-//     return (
-//         <>
-//             <Slider min={GENPARAMS.min.n1} max={GENPARAMS.max.n1} value={n1} onChange={setN1} label="Noise 1" />
-//             <Slider min={GENPARAMS.min.n2} max={GENPARAMS.max.n2} value={n2} onChange={setN2} label="Noise 2" />
-//             <Slider min={GENPARAMS.min.distortion} max={GENPARAMS.max.distortion} value={distortion} onChange={setDistortion} label="Distortion" />
-//             <Slider min={GENPARAMS.min.dotDiameter} max={GENPARAMS.max.dotDiameter} value={dotDiameter} onChange={setDotDiameter} label="Dot diameter" />
-//         </>
-//     );
-// }
 
 export function Row2_Sliders() {
     const showNoiseEditor = useAtomValue(ShowNoiseEditorAtom);
     return (
         <div className="flex">
             <div className="flex-1">
-                {/* <Sliders /> */}
                 <Sliders1 />
                 <Sliders2 />
                 <Sliders3 />
                 <Sliders4 />
             </div>
+            {/* Editors separator */}
             {showNoiseEditor && <div className="w-[1px] max-w-[1px] bg-purple-300"></div>}
             <div>
                 <Row2_NoiseEditor />
