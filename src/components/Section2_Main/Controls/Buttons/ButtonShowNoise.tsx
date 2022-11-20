aec40fdeef3c78d11b4e6dba336db9239d827615
Max Zakharzhevskiy <maxzz@msn.com>
2022-11-19 23:22:36 -0800

    WIP. top row. add IconCosine as component

--- src/components/Section2_Main/Controls/Buttons/ButtonShowNoise.tsx
+++ src/components/Section2_Main/Controls/Buttons/ButtonShowNoise.tsx
@@ -1,6 +1,7 @@
 import React from 'react';
 import { useSetAtom } from 'jotai';
 import { ShowNoiseEditorAtom } from '@/store';
+import { IconCosine } from '@/components/UI/Icons';
 
 export function ButtonShowNoise() {
     const setShowNoiseEditor = useSetAtom(ShowNoiseEditorAtom);
@@ -10,14 +11,8 @@ export function ButtonShowNoise() {
             title="Show/Hide noise parameters"
             onClick={() => setShowNoiseEditor(prev => !prev)}
         >
-            {IconCosine()}
+            <IconCosine />
         </div>
     );
-}
-function IconCosine() {
-    return <svg className="" viewBox="0 0 101 101" fill="none" stroke="currentColor" strokeWidth={4}>
-        <path d="M88.3 139c11.6-34 23.8-35.6 35 0 10.5 33.2 23.4 34 35 0" transform="translate(-72.8 -88)" />
-        <path d="M15.4 22.4v55.7M12.1 50.5h78.8" strokeWidth={2} />
-    </svg>;
 }
 
--- src/components/UI/Icons/index.tsx
+++ src/components/UI/Icons/index.tsx
@@ -48,3 +48,12 @@ export function IconSave({ className, ...rest }: SVGAttributes<SVGSVGElement>) {
         </svg>
     );
 }
+
+export function IconCosine({ className, ...rest }: SVGAttributes<SVGSVGElement>) {
+    return (
+        <svg className={classNames("fill-transparent stroke-current", className)} viewBox="0 0 101 101" {...rest}>
+            <path d="M88.3 139c11.6-34 23.8-35.6 35 0 10.5 33.2 23.4 34 35 0" transform="translate(-72.8 -88)" strokeWidth={4} />
+            <path d="M15.4 22.4v55.7M12.1 50.5h78.8" strokeWidth={2} />
+        </svg>
+    );
+}
