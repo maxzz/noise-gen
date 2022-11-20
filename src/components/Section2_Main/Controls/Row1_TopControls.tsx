1537c608b8737139c38a75d921f6d355ae66d45a
Max Zakharzhevskiy <maxzz@msn.com>
2022-11-19 23:52:27 -0800

    WIP. top row. attempt to generalize Button component atom

--- src/components/Section2_Main/Controls/Row1_TopControls.tsx
+++ src/components/Section2_Main/Controls/Row1_TopControls.tsx
@@ -1,17 +1,19 @@
 import React, { HTMLAttributes } from 'react';
-import { useAtom, useSetAtom } from 'jotai';
+import { Atom, PrimitiveAtom, useAtom, useSetAtom, WritableAtom } from 'jotai';
 import { GeneratePresetAtom, RandomSeedAtom, SeedAtom, ShowNoiseEditorAtom } from '@/store';
 import { ButtonChooseColorBg } from './Buttons/ButtonChooseColorBg';
 import { ButtonChooseColor } from './Buttons/ButtonChooseColor';
 import { classNames } from '@/utils';
 import { IconCosine } from '@/components/UI/Icons';
 
-function Button({ className, children, ...rest }: HTMLAttributes<HTMLButtonElement>) {
+function Button({ className, children, setAtom, onClick, ...rest }: {setAtom: WritableAtom<null, unknown> | WritableAtom<null, boolean>} &  HTMLAttributes<HTMLButtonElement>) {
+    const set = useSetAtom(setAtom);
     return (
         <button
             className={classNames(
                 "dark-frame-rounded text-gray-100 top-row-button-gradient active-scale no-active-ouline select-none", className,
             )}
+            onClick={onClick || set}
             {...rest}
         >
             {children}
@@ -19,32 +21,11 @@ function Button({ className, children, ...rest }: HTMLAttributes<HTMLButtonEleme
     );
 }
 
-function ButtonShowNoise() {
-    const setShowNoiseEditor = useSetAtom(ShowNoiseEditorAtom);
-    return (
-        <Button className="w-8 h-8" title="Show/Hide noise parameters" onClick={() => setShowNoiseEditor(prev => !prev)}>
-            <IconCosine />
-        </Button>
-    );
-}
-
-// export function ButtonShowNoise() {
-//     const setShowNoiseEditor = useSetAtom(ShowNoiseEditorAtom);
-//     return (
-//         <div
-//             className="dark-frame-rounded text-gray-100 top-row-button-gradient active-scale cursor-pointer"
-//             title="Show/Hide noise parameters"
-//             onClick={() => setShowNoiseEditor(prev => !prev)}
-//         >
-//             <IconCosine />
-//         </div>
-//     );
-// }
-
 export function Row1_TopControls() {
     const [seed, setSeed] = useAtom(SeedAtom);
     const setRansomSeed = useSetAtom(RandomSeedAtom);
     const generatePreset = useSetAtom(GeneratePresetAtom);
+    const setShowNoiseEditor = useSetAtom(ShowNoiseEditorAtom);
 
     return (
         <div className="h-8 flex space-x-1 text-sm">
@@ -54,17 +35,17 @@ export function Row1_TopControls() {
                 value={seed} onChange={(event) => setSeed(event.target.value)}
             />
 
-            <Button className="h-8 px-3 pb-0.5 uppercase" title="Generate random seed" onClick={setRansomSeed}>
+            <Button className="h-8 px-3 pb-0.5 uppercase" title="Generate random seed" setAtom={RandomSeedAtom}>
                 Random Seed
             </Button>
 
-            <Button className="h-8 px-3 pb-0.5 uppercase" title="Generate a random preset" onClick={generatePreset}>
+            <Button className="h-8 px-3 pb-0.5 uppercase" title="Generate a random preset" setAtom={GeneratePresetAtom}>
                 Preset
             </Button>
 
-            <div className="w-8 h-8 flex-none">
-                <ButtonShowNoise />
-            </div>
+            <Button className="w-8 h-8" title="Show/Hide noise parameters" setAtom={ShowNoiseEditorAtom} onClick={() => setShowNoiseEditor(prev => !prev)}>
+                <IconCosine />
+            </Button>
 
             <ButtonChooseColor className="w-8 h-8 top-row-button-gradient" />
             <ButtonChooseColorBg />
