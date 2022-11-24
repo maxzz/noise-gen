ea1e750b0c5bb2a807f17fecf7c3e03da0f2e95f
Max Zakharzhevskiy <maxzz@msn.com>
2022-11-23 16:35:52 -0800

    WIP. add Section3_Footer component

--- src/App.tsx
+++ src/App.tsx
@@ -2,6 +2,7 @@ import React from 'react';
 import { AppBackground } from './components/UI/AppBackground';
 import { Section1_Header } from './components/Section1_Header';
 import { Section2_Main } from './components/Section2_Main';
+import { Section3_Footer } from './components/Section3_Footer';
 import './App.scss';
 
 export function App() {
@@ -9,6 +10,7 @@ export function App() {
         <AppBackground>
             <Section1_Header />
             <Section2_Main />
+            <Section3_Footer />
         </AppBackground>
     );
 }
---
+++ src/components/Section3_Footer/index.tsx
@@ -0,0 +1,11 @@
+import React from 'react';
+
+export function Section3_Footer() {
+    return (
+        <div className="relative w-full">
+            <div className="absolute bottom-2 right-2">
+                me
+            </div>
+        </div>
+    );
+}
